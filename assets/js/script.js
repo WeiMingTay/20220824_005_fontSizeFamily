d198a4d32a39ba43de66070777b880019f169dce
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2022-08-26 22:45:38 +0200

    font-face angepasst

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 // font-size
 let fontSize = document.getElementById("fontSize");
 function changeFontSize() {
-  console.log(fontSize.value + "px");
-  document.getElementById("ausgabeFeld").style.fontSize = fontSize.value + "px";
+    document.getElementById("ausgabeFeld").style.fontSize = fontSize.value + "px";
 }
 // font-family
 
-function selectFontFamily() {
+/* function selectFontFamily() {
     let fontFamily = document.getElementById("fontFamily");
-  console.log(fontFamily.value);
-  document.getElementById("ausgabeFeld").style.fontFamily = fontFamily.value;
+    console.log(fontFamily.value);
+    document.getElementById("ausgabeFeld").style.fontFamily = fontFamily.value;
+} */
+
+// Kevin:
+function selectFontFamily() {
+	fontFamily = document.getElementById('fontFamily').value;
+	ausgabeFeld.style.fontFamily = fontFamily;
 }
 // text value
 let text = document.getElementById("textValue");
